8e0767a5e332eac7c63b2a8ba81fba004fb85771
Jason Kao <jason.kao@columbia.edu>
2022-05-01 23:01:42 -0400

    i gotchu

---
+++ src/andrew.js
@@ -0,0 +1,46 @@
+import svgs from './*.svg';
+
+console.log(svgs);
+
+var urls = ["cb9change", "manhattanchange", "nycchange"]
+// using d3 for convenience
+var main = document.querySelector("main");
+var scrolly = main.querySelector("#scrolly");
+var sticky = scrolly.querySelector(".sticky-thing");
+var article = scrolly.querySelector("article");
+var steps = article.querySelectorAll(".step");
+
+// initialize the scrollama
+var scroller = scrollama();
+
+// scrollama event handlers
+function handleStepEnter(response) {
+  // response = { element, direction, index }
+  var el = response.element;
+
+  // remove is-active from all steps
+  // then add is-active to this step
+  steps.forEach(step => step.classList.remove('is-active'));
+  el.classList.add('is-active');
+
+  // update graphic based on step
+  console.log(response.index);
+  console.log(document.getElementById("stick").getElementsByClassName("myimg")[0].src);
+  document.getElementById("stick").getElementsByClassName("myimg")[0].src = svgs[urls[response.index]];
+}
+
+function init() {
+  scroller
+    .setup({
+      step: "#scrolly article .step",
+      offset: 0.33,
+      debug: true
+    })
+    .onStepEnter(handleStepEnter);
+
+  // setup resize event
+  window.addEventListener("resize", scroller.resize);
+}
+
+// kick things off
+init();
